460baa56ee7fe4f84722d175aea77d2610d13f40
Harr-izle <harrietbuadee@gmail.com>
2024-09-09 16:24:06 +0000

    displays data from json

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toolList = document.querySelector(".tool-list");
+
+  fetch("./data/content.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok"); 
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((tool) => {
+        toolList.innerHTML += `
+                <li>
+                    <h3>${tool.name}</h3>
+                    <p>${tool.description}</p>
+                </li>
+            `;
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+});
